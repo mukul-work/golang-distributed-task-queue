5b5b5f55bc31a06cc7ed864df67ea29860e3932f
Mukul <mukulkashyap.business@gmail.com>
2026-08-27 15:48:53 +0530

    handler function for http requests

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/mukul-work/golang-distributed-task-queue/authutil"
 	"github.com/mukul-work/golang-distributed-task-queue/internal/dbgen"
 	"github.com/mukul-work/golang-distributed-task-queue/internal/service"
 	"github.com/mukul-work/golang-distributed-task-queue/models"
@@ -72,5 +73,74 @@ func (h *Handler) PostJob(w http.ResponseWriter, r *http.Request) {
 	writeJson(w, http.StatusOK, models.SuccessMessage{
 		Message: "Job registered successfully",
 		ID:      fmt.Sprintf("ID: %s", job.ID),
+	})
+}
+
+func (h *Handler) PostAdminApiKey(w http.ResponseWriter, r *http.Request) {
+
+	var req models.PostAPIRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		writeJson(w, http.StatusBadRequest, models.ErrorMessage{
+			Message: "Invalid Request",
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	key, err := authutil.KeyGenerator(req.Prefix)
+	if err != nil {
+		writeJson(w, http.StatusInternalServerError, models.ErrorMessage{
+			Message: "Failed to generate the API key",
+		})
+		return
+	}
+	hashKey := authutil.HashApiKeys(key)
+
+	newKey, err := h.q.CreateAPIKey(r.Context(), dbgen.CreateAPIKeyParams{
+		ID:      uuid.New().String(),
+		Name:    req.Name,
+		KeyHash: hashKey,
+	})
+
+	if err != nil {
+		writeJson(w, http.StatusInternalServerError, models.ErrorMessage{
+			Message: "Failedto generate API key",
+		})
+		return
+	}
+
+	writeJson(w, http.StatusCreated, map[string]any{
+		"id":         newKey.ID,
+		"name":       newKey.Name,
+		"key":        key,
+		"created_at": newKey.CreatedAt,
+		"warning":    "Save this key securely. It will not be visible again",
+	})
+}
+
+func (h *Handler) GetStatus(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	job, err := h.q.GetJob(r.Context(), id)
+	if err != nil {
+		writeJson(w, http.StatusNotFound, models.ErrorMessage{
+			Message: "ID could not be found",
+		})
+	}
+	writeJson(w, http.StatusOK, job)
+}
+
+func (h *Handler) GetApiKeys(w http.ResponseWriter, r *http.Request) {
+	keys, err := h.q.ListAPIKeys(r.Context())
+	if err != nil {
+		writeJson(w, http.StatusInternalServerError, models.ErrorMessage{
+			Message: "Could not List the API keys",
+			Error:   err.Error(),
+		})
+		return
+	}
+
+	writeJson(w, http.StatusOK, map[string]any{
+		"Message": "These are the keys",
+		"Keys":    keys,
 	})
 }
